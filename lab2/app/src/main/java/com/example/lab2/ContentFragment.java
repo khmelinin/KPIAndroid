0c02a7f6a55f9753a5d1568037f34bd633870b8b
AdmiN <khmelinin2001@gmail.com>
2021-11-25 18:08:46 +0200

    Lab2.1

--- lab2/app/src/main/java/com/example/lab2/ContentFragment.java
+++ lab2/app/src/main/java/com/example/lab2/ContentFragment.java
@@ -1,9 +1,11 @@
 package com.example.lab2;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +21,74 @@ public class ContentFragment extends Fragment {
         super(R.layout.fragment_content);
     }
 
+    interface OnFragmentSendDataListener {
+        void onSendData(String data);
+    }
+
+    private OnFragmentSendDataListener fragmentSendDataListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            fragmentSendDataListener = (OnFragmentSendDataListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " должен реализовывать интерфейс OnFragmentInteractionListener");
+        }
+    }
+
+
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_content, container, false);
+//        Button buttonOk = view.findViewById(R.id.buttonOk);
+//        CheckBox checkBoxS = view.findViewById(R.id.checkBoxS);
+//        CheckBox checkBoxP = view.findViewById(R.id.checkBoxP);
+//        RadioGroup radioGroup1 = view.findViewById(R.id.radioGroup1);
+//        RadioButton radioButtonCircle = view.findViewById(R.id.radioButtonCircle);
+//        RadioButton radioButtonRectangle = view.findViewById(R.id.radioButtonRectangle);
+//        RadioButton radioButtonTriangle = view.findViewById(R.id.radioButtonTriangle);
+//        //TextView textViewResult = view.findViewById(R.id.textViewResult);
+//
+//        buttonOk.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String selectedItem = "";
+//
+//                if (checkBoxS.isChecked()) {
+//                    if (radioButtonCircle.isChecked()) {
+//                        selectedItem += "S=Pi*r^2 ";
+//                    } else if (radioButtonRectangle.isChecked()) {
+//                        selectedItem += "S=a*b ";
+//                    } else if (radioButtonTriangle.isChecked()) {
+//                        selectedItem +="S=sqrt(p*(p−a)*(p−b)*(p−c)) ";
+//                    } else
+//                        selectedItem += "Choose the option! ";
+//                }
+//                if (checkBoxP.isChecked()) {
+//                    if (radioButtonCircle.isChecked()) {
+//                        selectedItem += "P=2*Pi*r ";
+//                    } else if (radioButtonRectangle.isChecked()) {
+//                        selectedItem += "P=2*(a+b) ";
+//                    } else if (radioButtonTriangle.isChecked()) {
+//                        selectedItem += "P=a+b+c ";
+//                    } else
+//                        selectedItem += "Choose the option! ";
+//                }
+//                if (!checkBoxP.isChecked() && !checkBoxS.isChecked()) {
+//                    Toast toast1 = Toast.makeText(view.getContext(), "Fill check boxes and radio buttons!", Toast.LENGTH_SHORT);
+//                    toast1.show();
+//                } else {
+//                    fragmentSendDataListener.onSendData(selectedItem);
+//                }
+//
+//            }
+//        });
+//        return view;
+//    }
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -29,37 +99,40 @@ public class ContentFragment extends Fragment {
         RadioButton radioButtonCircle = view.findViewById(R.id.radioButtonCircle);
         RadioButton radioButtonRectangle = view.findViewById(R.id.radioButtonRectangle);
         RadioButton radioButtonTriangle = view.findViewById(R.id.radioButtonTriangle);
-        TextView textViewResult = view.findViewById(R.id.textViewResult);
+
+
 
         buttonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textViewResult.setText("Result: ");
+                String selectedItem = "";
 
                 if (checkBoxS.isChecked()) {
                     if (radioButtonCircle.isChecked()) {
-                        textViewResult.setText(textViewResult.getText() + "S=Pi*r^2 ");
+                        selectedItem += "S=Pi*r^2 ";
                     } else if (radioButtonRectangle.isChecked()) {
-                        textViewResult.setText(textViewResult.getText() + "S=a*b ");
+                        selectedItem += "S=a*b ";
                     } else if (radioButtonTriangle.isChecked()) {
-                        textViewResult.setText(textViewResult.getText() + "S=sqrt(p*(p−a)*(p−b)*(p−c)) ");
+                        selectedItem +="S=sqrt(p*(p−a)*(p−b)*(p−c)) ";
                     } else
-                        textViewResult.setText(textViewResult.getText() + "Choose the option! ");
+                        selectedItem += "Choose the option! ";
                 }
                 if (checkBoxP.isChecked()) {
                     if (radioButtonCircle.isChecked()) {
-                        textViewResult.setText(textViewResult.getText() + "P=2*Pi*r ");
+                        selectedItem += "P=2*Pi*r ";
                     } else if (radioButtonRectangle.isChecked()) {
-                        textViewResult.setText(textViewResult.getText() + "P=2*(a+b) ");
+                        selectedItem += "P=2*(a+b) ";
                     } else if (radioButtonTriangle.isChecked()) {
-                        textViewResult.setText(textViewResult.getText() + "P=a+b+c ");
+                        selectedItem += "P=a+b+c ";
                     } else
-                        textViewResult.setText(textViewResult.getText() + "Choose the option! ");
+                        selectedItem += "Choose the option! ";
                 }
                 if (!checkBoxP.isChecked() && !checkBoxS.isChecked()) {
                     Toast toast1 = Toast.makeText(view.getContext(), "Fill check boxes and radio buttons!", Toast.LENGTH_SHORT);
                     toast1.show();
                 }
+                fragmentSendDataListener.onSendData(selectedItem);
+
             }
         });
     }
